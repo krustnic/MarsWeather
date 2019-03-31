ad05b512cec9b1323225cdb00fc92c3b0a50ab4b
avmityakov <avmityakov@gmail.com>
2019-03-31 17:02:59 +0300

    adjust for mobile

--- src/components/title.js
+++ src/components/title.js
@@ -2,6 +2,11 @@ import React from "react"
 
 import styles from "./title.module.css"
 
-const Title = ({ title }) => <div className={styles.title}>{title}</div>
+const Title = ({ title }) => (
+  <div className={styles.title}>
+    <div className={styles.main}>Погода на Марсе</div>
+    <div className={styles.details}>Elysium Planitia</div>
+  </div>
+)
 
 export default Title
